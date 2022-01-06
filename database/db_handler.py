8d94faaea1a36f2eb321b1f12fe593e03b63536f
heebeom_yang <takeny1998@gmail.com>
2022-01-06 19:34:02 +0900

    [REFACTOR] Separate DB Info to external config file

--- database/db_handler.py
+++ database/db_handler.py
@@ -1,14 +1,31 @@
 import pymysql as sql
+import configparser
 
-conn = sql.connect(host='www.tatine.kr',
-				port=3307,
-				database='data_analyzer',
-				user='jsp',
-				password='Gmlqja3520!')
+class DbHandler:
+	
+	def __init__(self):
+		# parse 'config.ini' file automatically
+		# please find 'config.ini' file or create it and place it in 'root' folder
+		# and enter your mysql or mariadb connection information
+		parser = configparser.ConfigParser()
+		parser.read('config.ini')
 
-cursor = conn.cursor()
+		db_props = parser["DATABASE"]
+		DB_HOST = db_props['HOST']
+		DB_PORT = db_props.getint('PORT')
+		DB_NAME = db_props['NAME']
+		DB_USER = db_props['USER']
+		DB_PW = db_props['PW']
 
-cursor.execute('SELECT * FROM user')
-users = cursor.fetchall()
+		conn = sql.connect(
+						host=DB_HOST,
+						port=DB_PORT,
+						database=DB_NAME,
+						user=DB_USER,
+						password=DB_PW)
+	
+		self.cursor = conn.cursor()
 
-print(users)
+	
+	
+db = DbHandler()
